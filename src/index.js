08648adfb2070501407c26e8b007401768464fb5
Yohamin <964989866.com@gmail.com>
2025-07-05 11:18:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";  // puedes crear un archivo vacío o con estilos globales
+import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./pages/Context/AuthContext";
+import { ProveedorCategoria } from "./pages/Context/ContextoCategoria";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ProveedorCategoria>
+          <App />
+        </ProveedorCategoria>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
