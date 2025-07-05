432fe640dc55cc7175f36ca45a781520ac2ebe2d
Yohamin <964989866.com@gmail.com>
2025-07-04 22:35:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";  // puedes crear un archivo vacío o con estilos globales
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ProveedorCategoria } from './pages/Context/ContextoCategoria';
+import { AuthProvider } from './pages/Context/AuthContext';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ProveedorCategoria>
+          <App />
+        </ProveedorCategoria>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
