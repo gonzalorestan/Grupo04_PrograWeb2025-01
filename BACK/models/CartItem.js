77fed35a9e7cfe833033f6a0e5de88c805e07d6d
gonzalorestan <gonzalo.restan@outlook.es>
2025-07-14 14:17:32 -0500

    probando

--- BACK/models/CartItem.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
-
-const CartItem = sequelize.define('CartItem', {
-  quantity: DataTypes.INTEGER
-});
-
-
-module.exports = CartItem;
--- BACK/models/OrderItem.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
-
-const OrderItem = sequelize.define('OrderItem', {
-  quantity: DataTypes.INTEGER,
-  price: DataTypes.FLOAT
-});
-
-module.exports = OrderItem;
